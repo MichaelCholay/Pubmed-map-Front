5e9985044b5dd13784fa49c515b37f3e55dbdf63
Cholay <michael.cholay@gmail.com>
2020-06-20 15:50:57 +0200

    add scale to map

--- my-app/src/app/map/map.component.ts
+++ my-app/src/app/map/map.component.ts
@@ -28,7 +28,7 @@ export class MapComponent implements OnInit {
     //   }
 
     // Set map on Paris
-    const mymap = L.map('mapid').setView([48.833, 2.333], 2);
+    const mymap = L.map('mapid').setView([48.833, 2.333], 2).addControl(L.control.scale( ));
 
     // List of layers
     var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
