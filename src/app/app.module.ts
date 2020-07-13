8dadbc409a8b2a775e62d6d53746afd0536a843b
Cholay <michael.cholay@gmail.com>
2020-07-14 00:23:18 +0200

    update of jwt and cleaning on verif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MapComponent } from './map/map.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { UserComponent } from './user/user.component';
+import { LoginComponent } from './common/auth/login/login.component';
+import { RegisterComponent } from './common/auth/register/register.component';
+import { UserComponent } from './common/auth/user/user.component';
 import { httpInterceptorProviders } from './common/model/auth/auth-interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
