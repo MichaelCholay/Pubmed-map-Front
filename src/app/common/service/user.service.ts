20d8dfa353fffe90bb40173516094269be647ce3
Cholay <michael.cholay@gmail.com>
2020-07-13 12:02:53 +0200

    update of url for deployment on heroku

--- src/app/common/service/user.service.ts
+++ src/app/common/service/user.service.ts
@@ -7,18 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
  
-<<<<<<< HEAD
-  private userUrl = './api-auth/test/user';
-  // private pmUrl = './auth-api/test/pm';
-  // private adminUrl = './auth-api/test/admin';
-=======
   // private userUrl = 'http://localhost:9997/api-auth/test/user';
   // private pmUrl = 'http://localhost:9997/api-auth/test/pm';
   // private adminUrl = 'http://localhost:9997/api-auth/test/admin';
   private userUrl = 'https://pubmed-map-spring-jwt.herokuapp.com/api-auth/test/user';
   private pmUrl = 'https://pubmed-map-spring-jwt.herokuapp.com/api-auth/test/pm';
   private adminUrl = 'https://pubmed-map-spring-jwt.herokuapp.com/api-auth/test/admin';
->>>>>>> dev_V0.5.5
  
   constructor(private http: HttpClient) { }
  
