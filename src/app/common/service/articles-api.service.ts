2481dc8999bf230014130e49a6597fec683edcec
Cholay <michael.cholay@gmail.com>
2020-07-15 21:03:15 +0200

    update of login component

--- src/app/common/service/articles-api.service.ts
+++ src/app/common/service/articles-api.service.ts
@@ -12,18 +12,19 @@ import { Geoloc } from '../model/geoloc';
 @Injectable()
 export class ArticlesApiService {
   articles: Article[];
-  localhostPortUrl: "http://localhost:9998"
+  localhostPortUrl: 'http://localhost:9998'
   hostSpring = 'https://pubmed-map-spring-article.herokuapp.com'
 
   constructor(private httpClient: HttpClient) { }
 
   getAllArticles(): Observable<Article[]> {
-    let allArticlesUrl = `${this.hostSpring}/article-api/public/articles`
+    let allArticlesUrl = `http://localhost:9998/article-api/public/articles`
+    console.log(allArticlesUrl)
     return this.httpClient.get<Article[]>(allArticlesUrl)
   }
 
   public getArticleByPmid(articleId) : Observable<Article>{
-    let url = `${this.hostSpring}/article-api/public/article/pmid/${articleId}`
+    let url = `http://localhost:9998/article-api/public/article/pmid/${articleId}`
     return this.httpClient.get<Article>(url);
   }
   
