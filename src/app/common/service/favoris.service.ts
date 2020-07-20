e3c5d67ea766c57dba3fa77c0746aa208f407855
Cholay <michael.cholay@gmail.com>
2020-07-21 01:42:38 +0200

    add environment variable in favoris.service

--- src/app/common/service/favoris.service.ts
+++ src/app/common/service/favoris.service.ts
@@ -2,23 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FavoriteArticle } from "../model/favoriteArticle";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavorisService {
-  private hostUrl = 'https://pubmed-map-spring-jwt.herokuapp.com';
-
+  // private hostUrl = 'https://pubmed-map-spring-jwt.herokuapp.com';
+  private jwtUrl = environment.jwtHost;
+  
 
   constructor(private http: HttpClient) { }
 
 //add to favorite
 addFavorite(username , articleId): Observable<any> {
-  return this.http.post(this.hostUrl+ "/api-auth/user/favoriteArticle/add/"+ username +"/"+  articleId,  {responseType:'text' as 'json'});
+  return this.http.post(this.jwtUrl+ "/api-auth/user/favoriteArticle/add/"+ username +"/"+  articleId,  {responseType:'text' as 'json'});
 }
 //retrieve list of favorites
 getAllFavorite(username): Observable<FavoriteArticle[]> {
-  return this.http.get<FavoriteArticle[]>(this.hostUrl + "/api-auth/user/favoriteArticle/findall/"+ username)
+  return this.http.get<FavoriteArticle[]>(this.jwtUrl + "/api-auth/user/favoriteArticle/findall/"+ username)
 }
 
 }
